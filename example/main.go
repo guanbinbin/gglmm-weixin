521008fdbfc8bf9f38bc4363578b8e12916e53dc
weihongguo <35117668@qq.com>
2020-02-27 17:52:00 +0800

    example

--- example/main.go
+++ example/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/weihongguo/gglmm"
 	account "github.com/weihongguo/gglmm-account"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+type ExampleService struct {
+}
+
+// CustomActions --
+func (service *ExampleService) CustomActions() ([]*gglmm.HTTPAction, error) {
+	actions := []*gglmm.HTTPAction{
+		gglmm.NewHTTPAction("/auth-info", service.AuthInfo, "GET"),
+	}
+	return actions, nil
+}
+
+// RESTAction --
+func (service *ExampleService) RESTAction(restAction gglmm.RESTAction) (*gglmm.HTTPAction, error) {
+	return nil, nil
+}
+
+// AuthInfo --
+func (service *ExampleService) AuthInfo(w http.ResponseWriter, r *http.Request) {
+	jwtUser := account.JWTUser{}
+	err := account.GetJWTClaimsSubjectFromRequest(r, &jwtUser)
+	if err != nil {
+		gglmm.NewFailResponse("claims subject").WriteJSON(w)
+		return
+	}
+	gglmm.NewSuccessResponse().WriteJSON(w)
+}
 
 func main() {
 	gglmm.RegisterGormDB("mysql", "example:123456@(127.0.0.1:3306)/example?charset=utf8mb4&parseTime=true&loc=UTC", 10, 5, 600)
@@ -30,6 +59,9 @@ func main() {
 
 	gglmm.RegisterHTTPHandler(account.NewWechatUserInfoService(31536000, "user"), "/wechat").
 		Middleware(account.JWTAuthMiddleware([]string{"user"}))
+
+	gglmm.RegisterHTTPHandler(&ExampleService{}, "/example").
+		Middleware(account.JWTAuthMiddleware([]string{"administrator", "user"}))
 
 	gglmm.ListenAndServe(":10000")
 }
